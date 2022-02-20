ad6639bf834369f33dc5566ea2d42ee52fa63359
maxwellharon <maxwellharon54@gmail.com>
2022-02-20 20:28:43 +0300

    webrtc with apirtc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Conversation />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conversation from "./components/Conversation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Conversation,
+  },
+};
 </script>
 
 <style>
